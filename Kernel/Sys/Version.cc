1b6d24263f95767cb274eec48293dba61b86db15
entiflags <fishka_2023@mail.ru>
2025-06-04 11:15:12 +0600

    Kernel: Version info

---
+++ Kernel/Sys/Version.cc
@@ -0,0 +1,13 @@
+namespace Kernel {
+
+const char *__kernel_name = "machina";
+
+int __kernel_version_major = 0;
+int __kernel_version_minor = 0;
+
+const char *__kernel_arch = "i686";
+
+const char *__kernel_build_date = __DATE__;
+const char *__kernel_build_time = __TIME__;
+
+}
